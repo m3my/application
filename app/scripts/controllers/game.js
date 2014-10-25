43706c564f34801ac98b3ba1983a33b9b6b4b280
Jan Mechtel <jmechtel@gmail.com>
2014-10-26 01:56:20 +0200

    actual movie data + card status

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -10,6 +10,10 @@
 angular.module('movieMemoryApp')
   .controller('GameCtrl', function ($scope, $routeParams, $firebase) {
 
+
+    var ref = new Firebase('https://popping-heat-9121.firebaseio.com/games/' + $routeParams.id);
+    $scope.game = $firebase(ref).$asObject();
+
     var ref = new Firebase('https://popping-heat-9121.firebaseio.com/games/' + $routeParams.id + '/players');
     $scope.players = $firebase(ref).$asArray();
 
@@ -22,24 +26,14 @@ angular.module('movieMemoryApp')
         }
       });
 
-    var ref = new Firebase('https://popping-heat-9121.firebaseio.com/movies');
-    var movies = $firebase(ref).$asArray();
-
-    movies.$loaded(
-        function(x) {
-            console.log('Hey' + x.length);
-        }, function(err) {
-            console.error(err);
-          });
-
     $scope.flipCard = function (card) {
-      if ($scope.game.flippedCards.length < 2) {
-        card.isFlipped = true;
+      if (($scope.game.flippedCards || []).length < 2) {
+        card.status = "flipped";
       }
     };
 
     $scope.$watch('game.cards', function (cards) {
-      $scope.game.flippedCards = _.where(cards, { isFlipped: true });
+      $scope.game.flippedCards = _.where(cards, { status: "flipped" });
     }, true);
 
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,34 @@
  * Controller of the movieMemoryApp
  */
 angular.module('movieMemoryApp')
-  .controller('MainCtrl', function ($scope, Game, $location) {
-    var $game = Game.$asArray();
-    $scope.play = function () {
-      var ref = new Firebase('https://popping-heat-9121.firebaseio.com/movies');
+  .controller('MainCtrl', function ($scope, Game, $location, $firebase) {
+
+    function createGame(movies) {
+      var n = movies.length;
+      var cards = [];
+      for (var i = 0; i < 24;++i) {
+        cards[i] = movies[Math.floor(Math.random() * n)];
+        cards[i].status = 'fresh';
+      }
+
       $game.$add(
           {
-            cards: [],
-            players: [{name: 'poop', score: 1}]
+            cards: cards
           }).then(function(ref) {
         var id = ref.name();
         $location.path('games/' + id);
+      });
+    }
+
+    var $game = Game.$asArray();
+    $scope.play = function () {
+      var ref = new Firebase('https://popping-heat-9121.firebaseio.com/movies');
+      var movies = $firebase(ref).$asArray();
+      movies.$loaded(
+          function(x) {
+              createGame(x);
+          }, function(err) {
+              console.error(err);
       });
     };
 
